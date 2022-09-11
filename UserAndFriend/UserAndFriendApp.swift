61d16882b638336f9a00737807a76c54780fc7c8
Andrii Kyrychenko <akyrych84@gmail.com>
2022-09-11 09:10:08 -0300

    Initial Commit

---
+++ UserAndFriend/UserAndFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAndFriendApp.swift
+//  UserAndFriend
+//
+//  Created by Andrii Kyrychenko on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct UserAndFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
